2527a665c1082611d8394e11e9e4a045df388d4b
unknown <naucalista@gmail.com>
2025-09-28 19:21:44 +0700

    Tugas 3 Pertemuan 4

---
+++ app/src/main/java/com/unsoed/informatikamobile/DaftarBukuActivity.kt
@@ -0,0 +1,36 @@
+package com.unsoed.informatikamobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.unsoed.informatikamobile.databinding.ActivityDaftarBukuBinding
+import com.unsoed.informatikamobile.ui.adapter.BookAdapter
+import com.unsoed.informatikamobile.viewmodel.MainViewModel
+
+class DaftarBukuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDaftarBukuBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val adapter = BookAdapter(books = emptyList())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDaftarBukuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+        viewModel.books.observe(this) {
+            adapter.setData(newBooks = it)
+        }
+
+        viewModel.fetchBooks(query = "kotlin programming")
+    }
+}
